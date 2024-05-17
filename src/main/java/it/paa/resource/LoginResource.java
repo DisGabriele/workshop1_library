3734dc55b2b8508cfbe0865f9862c4641a761b02
Gabriele <gabriele.distinti@gmail.com>
2024-05-17 10:03:29 +0200

    re addded login

---
+++ src/main/java/it/paa/resource/LoginResource.java
@@ -0,0 +1,53 @@
+package it.paa.resource;
+
+import io.quarkus.elytron.security.common.BcryptUtil;
+import it.paa.model.entity.User;
+import it.paa.service.UserService;
+import jakarta.annotation.security.PermitAll;
+import jakarta.inject.Inject;
+import jakarta.persistence.NoResultException;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.QueryParam;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+
+@Path("/public/login")
+public class LoginResource {
+
+    @Inject
+    UserService userService;
+
+    @GET
+    @PermitAll
+    public Response login(@QueryParam("username") String username, @QueryParam("password") String password) {
+        if(username == null || password == null || username.isEmpty() || password.isEmpty() || username.isBlank() || password.isBlank()) {
+            return Response.status(Response.Status.BAD_REQUEST)
+                    .type(MediaType.TEXT_PLAIN)
+                    .entity("Username and password cannot be enpty")
+                    .build();
+        }
+        User user;
+        try{
+            user = userService.getByName(username);
+        }
+        catch(NoResultException e) {
+            return Response.status(Response.Status.NOT_FOUND)
+                    .type(MediaType.TEXT_PLAIN)
+                    .entity("Username not found")
+                    .build();
+        }
+        if(BcryptUtil.matches(password, user.getPassword())) {
+            return Response.ok(username + " has logged in successfully")
+                    .type(MediaType.TEXT_PLAIN)
+                    .build();
+        }
+        else{
+            return Response.status(Response.Status.UNAUTHORIZED)
+                    .type(MediaType.TEXT_PLAIN)
+                    .entity("Password not correct!")
+                    .build();
+        }
+
+    }
+}
